a4fd4d53481abcf018b855f8ea8bc9909950ae64
user name <email@domain.com>
2021-11-01 02:17:55 +0400

    add API URL for google places to restrict the search in only Amman city

--- backend/src/places/places.service.ts
+++ backend/src/places/places.service.ts
@@ -21,6 +21,22 @@ export class PlacesService {
 
     }
 
+
+    /* We can restrict the search only in a specific city using location (longitude and latitude) and the radius 
+    
+    
+    for example if we want to search only in Amman we can use this Api Url :
+
+    https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Zar&
+    location=31.963158 35.930359
+    &radius=18000&
+    strictbounds=true&
+    key=AIzaSyCyObkvRpICwcyTi-dla_IzVFnP22gHcEk
+    
+    
+    
+    */
+
     
 
 }
